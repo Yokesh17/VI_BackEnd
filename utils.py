a1e78e3dd598ae9265b22f578b0526708c54ff70
Yokesh17 <yokeshragupathi2002@gmail.com>
2025-12-04 23:37:41 +0530

    hcuew

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ from fastapi import HTTPException, status
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 SECRET_KEY = "3213d8b8908c6224acad88f034b3a39eb46cdd3d40ae817a26beb930a1870353"  # 🔒 change in production
-ALGORITHM = "HS256"
+ALGORITHM = "HS256wevgw"
 
 def hash_password(password: str):
     return pwd_context.hash(password)
